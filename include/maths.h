b5191d3f600880fddb1fe1ea55ca7270b71a3786
Quentin <superretards22@gmail.com>
2025-08-28 11:59:02 +0200

    Add maths module with functions for angle conversion and position manipulation

---
+++ include/maths.h
@@ -0,0 +1,13 @@
+#ifndef MATHS_H_
+#define MATHS_H_
+
+#include "types.h"
+#include <lapin.h>
+
+double deg_to_rads(double degrees);
+t_bunny_position accurate_to_position(t_accurate_pos accurate_pos);
+t_accurate_pos position_to_accurate(t_bunny_position pos);
+float get_ratio(int a, int b, int x);
+int get_value(int a, int b, float ratio);
+
+#endif /* !MATHS_H_ */
---
+++ src/maths.c
@@ -0,0 +1,46 @@
+#include "maths.h"
+
+t_bunny_position accurate_to_position(t_accurate_pos accurate_pos)
+{
+    t_bunny_position pos;
+
+    pos.x = accurate_pos.x;
+    pos.y = accurate_pos.y;
+    return pos;
+}
+
+t_accurate_pos position_to_accurate(t_bunny_position pos)
+{
+    t_accurate_pos accurate_pos;
+
+    accurate_pos.x = pos.x;
+    accurate_pos.y = pos.y;
+    return accurate_pos;
+}
+
+double deg_to_rads(double degrees)
+{
+    return (degrees * (2 * M_PI)) / 360;
+}
+
+float get_ratio(int a, int b, int x)
+{
+    float ratio;
+    float tmp;
+
+    if (a == b) {
+        return 1;
+    }
+    tmp = (b - a);
+    return (ratio = (x - a) / tmp);
+}
+
+int get_value(int a, int b, float ratio)
+{
+    float tmp;
+    int value;
+
+    tmp = (ratio * (b - a) + a);
+    value = tmp;
+    return (value);
+}
