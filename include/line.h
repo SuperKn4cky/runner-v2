f55a749773aea3c80455352709de41c564a9cc6d
Quentin <superretards22@gmail.com>
2025-08-29 04:18:41 +0200

    add old raycast system

---
+++ include/line.h
@@ -0,0 +1,17 @@
+#ifndef LINE_H_
+#define LINE_H_
+
+#include <lapin.h>
+
+struct line {
+    int width_diff;
+    int height_diff;
+    float ratio;
+    t_bunny_position position;
+    t_bunny_position pos_a;
+    t_bunny_position pos_b;
+    t_bunny_position good_pos_a;
+    t_bunny_position good_pos_b;
+};
+
+#endif // LINE_H_
